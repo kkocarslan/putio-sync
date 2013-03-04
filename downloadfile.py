2668f6655b09e02ec4f30d999cd96679648137be
Emre Odabas <emre@golge.net>
2013-03-04 11:53:26 +0200

    return code handling corrected, wget path bind to env command

--- downloadfile.py
+++ downloadfile.py
@@ -6,15 +6,15 @@ def downloadfile(url, file_name, target_dir, size_expected, limit_rate):
     #options = "-t 10 --max-connection-per-server=4 --file-allocation=falloc --max-download-limit=900K -c -s 4 --summary-interval=20 --lowest-speed-limit=100K"
     #retcode = subprocess.call("/usr/bin/aria2c " + options + " \"" + url + "\" -d \"" + target_dir + "\" -o \"" + file_name + "\"", shell=True)
     options = "--continue --limit-rate=" + limit_rate + " --progress=dot:mega"
-    cmd = "/usr/bin/wget " + options + " '" + url + "' -O '" + target_dir + "/" + file_name+"' 2>&1"
+    cmd = "env wget " + options + " '" + url + "' -O '" + target_dir + "/" + file_name+"' 2>&1"
     print cmd
     retcode = subprocess.call(cmd, shell=True)
-    if retcode > 0:
-      print >>sys.stderr, "Child was terminated by signal", -retcode
-      return False
-    else:
+    if retcode == 0:
       print >>sys.stderr, "Child returned", retcode
       return True
+    else:
+      print >>sys.stderr, "Child was terminated by signal", -retcode
+      return False
   except OSError as e:
     print >>sys.stderr, "Execution failed:", e
     return False
